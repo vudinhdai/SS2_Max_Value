fa4175a0e09cfd58fc4ada66efa6f2ad8b12aab4
vudinhdai <vudinhdai1999hnue@gmail.com>
2019-07-28 23:16:16 +0700

    tim gia tri lon nhat

---
+++ src/MaxValue.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MaxValue {
+    public static void main(String[] args) {
+        //Khai báo các biến, nhập và kiểm tra kích thước mảng
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("enter a size :");
+            size = scanner.nextInt();
+            if (size > 20) {
+                System.out.println("Size should not exceed 20 ");
+            }
+        } while (size > 20);
+
+
+        // Nhập giá trị cho các phần tử của mảng
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.println("enter element" + (i + 1) + ":");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+
+        // In ra danh sách tài sản đã nhập
+        System.out.println("enter Property list:");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+
+        }
+        // Duyệt các phần tử trong mảng để tìm GTLN và vị trí
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is:" + max + " ,at position " + index);
+    }
+}
